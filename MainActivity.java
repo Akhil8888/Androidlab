5920c87ee8486008091e9f6d8aae6e534005186c
Akhil8888 <77248343+Akhil8888@users.noreply.github.com>
2022-01-01 23:59:26 +0530

    Add files via upload
    lifecycle

--- MainActivity.java
+++ MainActivity.java
@@ -1,56 +1,53 @@
-package com.example.myapplication;
+package com.example.lifecycle;
 
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
-import android.view.View;
-import android.widget.Button;
-import android.widget.EditText;
 import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
-    EditText username;
-    EditText password;
-    Button login;
-    String name="admin";
-    String ps="1234";
-
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-
-
-        username =findViewById(R.id.username1);
-        password=findViewById(R.id.password1);
-        login=findViewById(R.id.login1);
-
-
-        login.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                String inpname = username.getText().toString();
-                String pwd = password.getText().toString();
-
-                if (inpname.isEmpty() || pwd.isEmpty()) {
-                    Toast.makeText(MainActivity.this, "Fields are emplty ", Toast.LENGTH_SHORT).show();
-
-                } else {
-                    if (inpname.equals(name) && pwd.equals(ps)) {
-                        Toast.makeText(MainActivity.this, "Success", Toast.LENGTH_SHORT).show();
-                    }
-                }
-                 if(!inpname.equals(name)) {
-                    Toast.makeText(MainActivity.this, "Username Incorrect", Toast.LENGTH_SHORT).show();
-
-                } else if (!pwd.equals(ps)) {
-                    Toast.makeText(MainActivity.this, "Incorrect Password", Toast.LENGTH_SHORT).show();
-                }
-
-
-            }
-
-            });
-            }
+        Toast.makeText(MainActivity.this, "on create ", Toast.LENGTH_SHORT).show();
     }
+    protected void onStart()
+    {
+        super.onStart();
+        Toast.makeText(MainActivity.this, "on start", Toast.LENGTH_SHORT).show();
+    }
+    @Override
+    protected void onResume()
+    {
+        super.onResume();
+        Toast.makeText(MainActivity.this, "on Resume", Toast.LENGTH_SHORT).show();
+
+    }
+    protected  void onPause()
+    {
+        super.onPause();
+        Toast.makeText(MainActivity.this, "on pause ", Toast.LENGTH_SHORT).show();
+    }
+
+    @Override
+    protected  void onStop()
+    {
+        super.onStop();
+        Toast.makeText(MainActivity.this, "on Stop ", Toast.LENGTH_SHORT).show();
+    }
+    protected void onDestroy()
+    {
+        super.onDestroy();
+        Toast.makeText(MainActivity.this, "on Destroy", Toast.LENGTH_SHORT).show();
+
+
+
+    }
+    protected void onRestart()
+    {
+        super.onRestart();
+        Toast.makeText(MainActivity.this, "on Restart", Toast.LENGTH_SHORT).show();
+    }
+}
